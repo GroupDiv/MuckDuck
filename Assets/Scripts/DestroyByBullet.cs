8497c77dbdef50c8bf15f8f8f7517b07f83bb5c6
Taylor Bauer <taylorbauer@ku.edu>
2019-04-03 09:56:02 -0500

    Bullets destroy enemies

---
+++ Assets/Scripts/DestroyByBullet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByBullet  : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // Destroy everything that leaves the trigger
+        if (other.gameObject.tag == "Bullet"){
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
