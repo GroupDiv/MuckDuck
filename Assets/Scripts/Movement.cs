346369b66342d26778843514e6cd6dc478748dc7
Marcus Leong <mwleong25@gmail.com>
2019-04-04 00:16:57 -0500

    Wave follows player

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Transform Character; // Target Object to follow
+    public float speed; // Enemy speed
+    private Vector2 directionOfCharacter;
+    void Update()
+    {
+        directionOfCharacter = Character.transform.position - transform.position;
+        directionOfCharacter = directionOfCharacter.normalized;    // Get Direction to Move Towards
+        transform.Translate(directionOfCharacter * speed, Space.World);
+    }
+}
