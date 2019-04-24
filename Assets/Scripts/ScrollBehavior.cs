4e04f06ef12e49dbfefdf1664271c04835e8e134
Ryan Pierce <rcp07@ku.edu>
2019-04-24 08:25:23 -0500

    Scrolling background works! Speed can be adjusted by changing the scrollOffsetSpeed variable in the ScrollBehavior script.

---
+++ Assets/Scripts/ScrollBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollBehavior : MonoBehaviour
+{
+    public float scrollOffsetSpeed = 20.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        MeshRenderer BGmr = GetComponent<MeshRenderer>();
+
+        Material BGMaterial = BGmr.material;
+
+        Vector2 offset = BGMaterial.mainTextureOffset;
+
+        offset.y += Time.deltaTime / scrollOffsetSpeed;
+
+        BGMaterial.mainTextureOffset = offset;
+    }
+}
