9e8d1b5f28fa36a069d48e1e8c7e6687878ec20b
Marcus Leong <mwleong25@gmail.com>
2019-04-30 11:35:43 -0500

    Working boss that shoots projectiles

--- Assets/Scripts/QueenBauss.cs
+++ Assets/Scripts/QueenBauss.cs
@@ -1,9 +1,10 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class QueenBauss : MonoBehaviour
 {
     //! Target Object to follow (usually player)
-    private GameObject Character; 
+    private GameObject Character;
 
     //! Enemy speed settable from Unity editor
     public float speed;
@@ -20,11 +21,30 @@ public class QueenBauss : MonoBehaviour
     //! Tracks how much damage the boss has received, when damage == health, the boss dies
     private int damage;
 
+    //! Defines which projectile the boss shoots
+    public GameObject projectile;
+
+    //! A reference to the object where the boss's projectiles spawn (relative to boss position)
+    public Transform projectileSpawn;
+
+
+    //! How many projectiles to spawn in each wave
+    public int hazardCount;
+
+    //! How long to wait between spawns
+    public float spawnWait;
+
+    //! How long to wait in between spawns
+    public float waveWait;
+
+
     private bool dirRight;
 
-    void Start() {
+    void Start()
+    {
         dirRight = true;
         damage = 0;
+        StartCoroutine(SpawnProjectiles());
     }
     /*!
     * @pre: none
@@ -46,14 +66,16 @@ public class QueenBauss : MonoBehaviour
     void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("Something is touching me");
-        if (other.gameObject.tag == "Bullet"){
+        if (other.gameObject.tag == "Bullet")
+        {
             Destroy(other.gameObject);
             //shake.EnemyCameraShake();
             damage++;
         }
     }
 
-    void Update() {
+    void Update()
+    {
         if (damage == health)
         {
             Debug.Log("I'm a hit");
@@ -80,6 +102,25 @@ public class QueenBauss : MonoBehaviour
         if (transform.position.x <= -boundary)
         {
             dirRight = true;
+        }
+    }
+
+    IEnumerator SpawnProjectiles()
+    {
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                Quaternion spawnRotation = Quaternion.identity;
+                var obj = Instantiate(projectile, projectileSpawn.position, projectileSpawn.rotation);
+                yield return new WaitForSeconds(spawnWait);
+                if(health == (damage-1))
+                {
+                    spawnWait *= (float).25;
+                    waveWait *= (float).35;
+                }
+            }
+            yield return new WaitForSeconds(waveWait);
         }
     }
 }
