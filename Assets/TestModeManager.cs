557aafbe1f4da1eca448c2047c0bccbeddbd47e0
Taylor Bauer <taylorbauer@ku.edu>
2019-04-30 15:23:51 -0500

    Test mode up and working!

---
+++ Assets/TestModeManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestModeManager : MonoBehaviour
+{
+
+    public bool testFlag;
+    // Start is called before the first frame update
+    void Start()
+    {
+        testFlag = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
